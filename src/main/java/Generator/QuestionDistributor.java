e4dedf3681a13ce35f1fdd5190310d6209951823
airsquared <36649395+airsquared@users.noreply.github.com>
2023-12-01 19:43:48 -0800

    Fix errors

--- src/main/java/Generator/QuestionDistributor.java
+++ src/main/java/Generator/QuestionDistributor.java
@@ -1,26 +1,32 @@
 package Generator;
+
 import Format.QuestionPrompter;
-import Generator.ChatResponse;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class QuestionDistributor {
 
     public void distribute(String filePath, String targetLang, String srcLang, String topic){
-        ChatResponse gen = new ChatResponse();
-        QuestionPrompter prompter = new QuestionPrompter();
+        var gen = new ChatResponse();
+        var prompter = new QuestionPrompter();
         String prompt = prompter.createPrompt(targetLang, srcLang, topic);
         String questions = gen.getQuestion(prompt);
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            writer.write(questions);
+        try {
+            var path = Path.of(filePath);
+            Files.createDirectories(path.getParent());
+            Files.writeString(path, questions);
         } catch (IOException e) {
-            System.out.println("Error writing JSON to the file: " + e.getMessage());
+            System.err.println("Error writing JSON to the file: " + e.getMessage());
+            e.printStackTrace();
         }
     }
     public static void main(String[] args){
-        QuestionDistributor distributor = new QuestionDistributor();
+        var distributor = new QuestionDistributor();
         distributor.distribute("src/main/data/JavaToPython/modules/Essentials/Printing.json", "python", "java", "printing statements");
         //distributor.distribute("src/main/data/PythonToJava/modules/Essentials/Printing.json", "java", "python", "printing statements");
     }
