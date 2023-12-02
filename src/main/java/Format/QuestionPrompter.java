56b142107520523d4f1c81d26e5fb1997bac3171
airsquared <36649395+airsquared@users.noreply.github.com>
2023-12-01 22:07:57 -0800

    Minor prompt modification

--- src/main/java/Format/QuestionPrompter.java
+++ src/main/java/Format/QuestionPrompter.java
@@ -2,12 +2,15 @@ package Format;
 
 public class QuestionPrompter {
     public String createPrompt(String targetLang, String srcLang, String topic){
-        var prompt = "Create 10 questions asking to translate a line of " + srcLang + "language code to " + targetLang + "language, with code ONLY on the topic of " + topic + ". Include wrong answers. Format the response as JSON in the shape of \n" +
-                "[{\n" +
-                "  \"question\": \"What is the translation: int x = 5; \",\n" +
-                "  \"answer\": \"x = 5\",\n" +
-                "  \"wrongAnswers\": [\"let x = 5\", \"var x = 5 \", \"integer = 5;\"]\n" +
-                "}];";
+        var prompt =
+                "Create 10 questions asking to translate a line of " + srcLang + " language code to " + targetLang + " language, with code ONLY on the topic of " + topic +
+                """
+                . Include wrong answers. Format the response as JSON in the shape of
+                [{
+                  "question": "What is the translation: int x = 5; ",
+                  "answer": "x = 5",
+                  "wrongAnswers": ["let x = 5", "var x = 5 ", "integer = 5;"]
+                }];""";
         return prompt;
     }
 }
