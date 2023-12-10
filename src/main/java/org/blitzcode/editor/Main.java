63719e380e4474e31630f83d33c589bccdd919b9
airsquared <36649395+airsquared@users.noreply.github.com>
2023-12-09 17:50:16 -0800

    Add Spring Shell

--- src/main/java/org/blitzcode/editor/Main.java
+++ src/main/java/org/blitzcode/editor/Main.java
@@ -2,8 +2,10 @@ package org.blitzcode.editor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
+@CommandScan("org.blitzcode.editor.commands")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
---
+++ src/main/java/org/blitzcode/editor/commands/SaveCommand.java
@@ -0,0 +1,7 @@
+package org.blitzcode.editor.commands;
+
+import org.springframework.shell.command.annotation.Command;
+
+@Command(command = "save")
+public class SaveCommand {
+}
