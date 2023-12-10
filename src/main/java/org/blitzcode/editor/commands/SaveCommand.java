b2516a9168e0f984e21ef0f8ca424922afb04c56
airsquared <36649395+airsquared@users.noreply.github.com>
2023-12-10 01:11:13 -0800

    Add commented code

--- src/main/java/org/blitzcode/editor/commands/SaveCommand.java
+++ src/main/java/org/blitzcode/editor/commands/SaveCommand.java
@@ -152,4 +152,104 @@ public class SaveCommand {
     }
 
     private record QuestionItem(String question, String answer, List<String> wrongAnswers) {}
+
+    //    @GetMapping(path = "/updateJsonFiles")
+//    public void getQuestions(JwtAuthenticationToken token) {
+    // Commenting these out becuase they need more testing before making it available
+//        ObjectMapper mapper = new ObjectMapper();
+//        try{
+//            URL url = getClass().getResource("/Courses");
+//            Path path = Paths.get(url.toURI());
+//            Stream<Path> stream = Files.walk(path, 1);
+//            ArrayList<String> courses = new ArrayList<>();
+//            stream.filter(Files::isDirectory)
+//                    .map(p -> p.getFileName().toString())
+//                    .filter(filename -> filename.contains("To"))
+//                    .forEach(courses::add);
+//
+//            for(String course: courses){
+//                ArrayList<String> modules = new ArrayList<>();
+//                url = getClass().getResource("/Courses/" + course + "/modules");
+//                path = Paths.get(url.toURI());
+//                stream = Files.walk(path, 1);
+//                stream.filter(Files::isDirectory)
+//                        .map(p -> p.getFileName().toString())
+//                        .filter(filename -> !filename.contains("modules"))
+//                        .forEach(modules::add);
+//                Language baseLanguage = Language.valueOf(course.split("To")[0].toUpperCase());
+//                Language targetLanguage = Language.valueOf(course.split("To")[1].toUpperCase());
+//                for(String module: modules){
+//                    ArrayList<String> lessons = new ArrayList<>();
+//                    url = getClass().getResource("/Courses/" + course + "/modules/" + module);
+//                    path = Paths.get(url.toURI());
+//                    stream = Files.walk(path, 1);
+//                    stream.filter(Files::isRegularFile)
+//                            .map(p -> p.getFileName().toString())
+//                            .filter(filename -> !filename.contains(module.toString()))
+//                            .forEach(lessons::add);
+//                    // Create a Module object for the database
+//                    Module newModule;
+//                    Module databaseModule = moduleController.findModuleByName(module.toString());
+//                    if (databaseModule != null) {
+//                        newModule = databaseModule;
+//                    }else{
+//                        newModule = new Module();
+//                        newModule.setName(module.toString());
+//                    }
+//                    ArrayList<Lesson> newLessons = new ArrayList<>();
+//                    for(String lesson: lessons){
+//                        Lesson newLesson;
+//                        String lessonName = lesson.split("\\.")[0];
+//                        Lesson databaseLesson = moduleController.findLessonByName(lessonName);
+//                        if(databaseModule == null){
+//                            newLesson = new Lesson();
+//                            newLesson.setName(lesson.split("\\.")[0]);
+//                            newLesson.setModule(newModule);
+//                            newLesson.setPoints(1);
+//                        }else{
+//                            newLesson = databaseLesson;
+//                        }
+//
+//                        InputStream inputStream = getClass().getResourceAsStream("/Courses/" + course + "/modules/" + module + "/" + lesson);
+//                        if (inputStream == null) {
+//                            throw new FileNotFoundException("Resource not found: /JavaToPython/modules/Essentials/Printing.json");
+//                        }
+//                        List<QuestionItem> quizItems = mapper.readValue(inputStream, new TypeReference<List<QuestionItem>>() {});
+//
+//                        Question[] questions = new Question[quizItems.size()];
+//                        ArrayList<org.blitzcode.api.model.Question> questionDatabaseList = new ArrayList<>();
+//
+//                        // Now you can use the quizItems list
+//                        for (int i = 0; i < questions.length; i++) {
+//                            QuestionItem item = quizItems.get(i);
+//                            int answerIndex = insertStringAtRandomIndex(item.wrongAnswers, item.answer);
+//                            String[] arr = item.wrongAnswers.toArray(new String[0]);
+//                            questions[i] = new Question(item.question, answerIndex, arr);
+//                            org.blitzcode.api.model.Question newQuestion = new org.blitzcode.api.model.Question();
+//                            newQuestion.setText(item.question);
+//                            newQuestion.setBaseLanguage(baseLanguage);
+//                            newQuestion.setTargetLanguage(targetLanguage);
+//                            newQuestion.setCorrectAnswer(item.answer);
+//                            newQuestion.setWrongOptions(item.getWrongAnswers());
+//                            newQuestion.setLesson(newLesson);
+//                            questionDatabaseList.add(newQuestion);
+//                        }
+//                        if(newLesson.getQuestions() != null){
+//                            questionDatabaseList.addAll(newLesson.getQuestions());
+//                        }
+//                        newLesson.setQuestions(questionDatabaseList);
+//                        newLessons.add(newLesson);
+//                    }
+//                    newModule.setLessons(newLessons);
+//                    moduleController.createModule(newModule);
+//                }
+//            }
+//
+////            Files.walk(path, 5).forEach(p -> logger.error(p.toString()));
+//        }catch (URISyntaxException e){
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
